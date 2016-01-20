a8baa66aaaec281d500cc301cff4c5f95edbfe57
Billy Farroll <watanight@hotmail.co.uk>
2016-01-20 16:05:57 +0000

    Create indexsfp.php

---
+++ indexsfp.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Single Parent Fun</title>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css">
+    
+</head>
+
+<body>
+    
+    <img class="logo" src="Images/landing-page-02.png" height="370" width="900">
+         
+ <div class="sticker"></div>
+    
+    
+         <div class="h1">
+              <h1 class="heading1">For the Better!</h1>
+         </div>
+       <div class="h2">
+            <h2 class="heading2">Single Parent Fun wants your help!</h2>
+    </div>
+<div class="container1">
+    <p class="paragraph1">We will be in contact for you to help us build the all new Single Parent Fun Website</p>
+    </div>
+
+</body>
+
+<footer>
+<div class="email">
+<p class="paragraph2">Insert your e-mail address and we will keep you updated!</p>
+</div>
+</footer>
+
+</html>
